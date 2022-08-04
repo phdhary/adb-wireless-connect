e1d68b1c02063f60faf4aa6b009d122a859ff435
Hary Prihadi <phdhary@gmail.com>
2022-08-04 10:33:40 +0700

    finish script

---
+++ main.sh
@@ -0,0 +1,22 @@
+adb_tcpip ()
+{
+    adb tcpip 5555;
+}
+
+adb_find_device_ip ()
+{
+    adb shell ip addr show wlan0 | grep "inet\s" | awk '{print $2}' | awk -F'/' '{print $1}';
+}
+
+adb_connect ()
+{
+  device_ip="$(adb_find_device_ip)";
+  adb connect $device_ip;
+}
+
+main ()
+{
+  adb_connect;
+}
+
+main
